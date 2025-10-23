d3f8b5ecb459d0efd88ed756c06d64cc578041d3
Wasima230Sultana <wasimasultana230@gmail.com>
2025-10-23 09:30:56 +0600

    Add sidebar

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,6 +4,9 @@ const Footer = () => {
     return (
         <div>
             <p>Thank you for visiting our website. </p>
+            <a href="/">Terms</a>
+            <a href="/">Privacy</a>
+            <a href="/fb">Facebook</a>
         </div>
     );
 };
--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import Header from './Header';
 import { Outlet } from 'react-router';
 import Footer from './Footer';
-
+import Sidebar from './Sidebar';
+import './Root.css'
 const Root = () => {
     return (
         <div>
             <Header></Header>
+            <div className='root-main'>
+               <Sidebar></Sidebar>
             <Outlet></Outlet>
+
+            </div>
             <Footer></Footer>
         </div>
     );

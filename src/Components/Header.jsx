416c1c20776e37c1a8096dac6da3282e9e059381
Wasima230Sultana <wasimasultana230@gmail.com>
2025-10-23 08:57:36 +0600

    Add links in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
-
+import { Link } from 'react-router';
+import './Header.css'
 const Header = () => {
     return (
         <div>
             <h3>This is header</h3>
+
+            <nav>
+                {/* <a href="/">Home</a>
+                <a href="/mobiles">Mobiles</a>
+                <a href="/laptops">Laptops</a> */}
+                
+                <Link to={"/"}>Home</Link>
+                <Link to={"/mobiles"}>Mobiles</Link>
+                <Link to={"/laptops"}>Laptops</Link>
+                <Link to={"/"}>Home</Link>
+
+
+            </nav>
         </div>
     );
 };
